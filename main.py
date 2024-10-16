b151fa224b00e605b7934b8c3d3a92dbe50199a7
andrhars <andreas@haarsaker.net>
2024-10-16 10:52:24 +0200

    finished file

---
+++ main.py
@@ -0,0 +1,43 @@
+def main():
+    path = "books/frankenstein.txt"
+    with open(path) as f:
+        file_contents = f.read()
+
+        print(f"--- Begin report of {path} ---")
+        print("")
+        print(f"{count_words(file_contents)} words found in the document")
+        print("")
+
+        for char, count in sort(count_characters(file_contents)):
+            print(f"The '{char}' character was found {count} times")
+        
+        print("")
+        print("--- End report ---")
+
+        
+        
+        #print(dict_of_dicts(file_contents))
+
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def count_characters(text):
+    lower_characters = text.lower()
+    characters = {}
+    for c in lower_characters:
+        if c.isalpha() :
+            characters[c] = characters.get(c, 0) + 1
+    return(characters)
+
+def sort(dictionary):
+    dict_list = [(char, count) for char, count in dictionary.items()]
+    dict_list.sort(key=lambda x: x[1], reverse=True)
+    return(dict_list)
+
+
+
+
+
+main()
